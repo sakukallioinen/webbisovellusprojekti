f349fa85f503339c7d68b3a2f2ab353cd7126efd
sakukallioinen <saku.kallioinen@hotmail.com>
2024-10-19 16:48:25 +0300

    email

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
 
+    // Lähetä EmailJS:n kautta
+    emailjs.send("service_dt83era", "template_xi7j7x6", {
+        name: name,
+        email: email,
+        message: message
+    })
+
     // Tarkista, että kaikki kentät ovat täytetty
     if (name && email && message) {
         alert("Kiitos, viestisi on lähetetty!");
